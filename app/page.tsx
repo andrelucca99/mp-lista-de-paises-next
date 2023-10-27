75bec3bbee9a79573cfd71d13a6a03b63138ed12
Andre Lucas <andrelucca99@gmail.com>
2023-10-27 12:20:09 -0300

    feat: fazendo chamada a api com server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+type Country = {
+  name: {
+    common: string
+  }
+}
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
   return (
     <section className="container flex w-full">
-      <h1>home</h1>
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
     </section>
   )
 }
