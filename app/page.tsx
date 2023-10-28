8e6a31925599945479a43c659256282722667bd1
Andre Lucas <andrelucca99@gmail.com>
2023-10-28 11:35:53 -0300

    feat: add rotas para paises especificos

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
-type Country = {
+export type Country = {
   name: {
     common: string
   };
@@ -14,7 +15,14 @@ type Country = {
   flags: {
     svg: string;
     alt: string;
-  }
+  };
+  capital: string;
+  region: string;
+  subregion: string;
+  population: number;
+  languages: {
+    [key: string]: string;
+  };
 }
 
 async function getCountries(): Promise<Country[]> {
@@ -28,12 +36,14 @@ export default async function Home() {
   return (
     <section className="container grid grid-cols-5 w-full gap-2 mt-16">
       {countries.map((country) => (
-        <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl" key={country.name.common}>
-          <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
-            <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
-          </div>
-          <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
-        </article>
+        <Link key={country.name.common} href={`/pais/${country.name.common}`}>
+          <article className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl">
+            <div className="relative w-full h-40 p-2 overflow-hidden rounded-xl">
+              <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
+            </div>
+            <h1 className="font-bold text-xl text-center mt-1">{country.translations.por.common}</h1>
+          </article>
+        </Link>
       ))}
     </section>
   )
---
+++ app/pais/[name]/page.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import type { Country } from "@/app/page";
+import Link from "next/link";
+import Image from "next/image";
+
+async function getCountryByName(name: string): Promise<Country> {
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}/?fullText=true`);
+  return (await response.json())[0];
+}
+
+export default async function CountryPage({
+  params: { name },
+}: {
+  params: { name: string };
+}) {
+  const country = await getCountryByName(name);
+
+  const formatter = Intl.NumberFormat("en", { notation: "compact" });
+
+  return (
+    <section className="flex flex-col container">
+      <h1 className="text-5xl text-center font-bold text-gray-800 my-16">
+        {country.translations.por.common}
+      </h1>
+      <Link className="flex items-center py-2" href="/">
+        <Image src="/arrow-back.svg" alt="ícone de seta para voltar" width={24} height={24} />
+        Voltar
+      </Link>
+      <article className="flex justify-between min-w-full p-10 bg-white rounded-xl">
+        <section>
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🏙️Capital:</b> {country.capital}
+          </h2>
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🗺️Continente:</b> {country.region} - {country.subregion}
+          </h2>
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🧑‍🤝‍🧑População:</b> {formatter.format(country.population)}
+          </h2>
+          <h2 className="text-xl text-gray-800 mt-3">
+            <b>🗣️Línguas faladas:</b> {Object.values(country.languages).map((language) => (
+              <span
+                key={language}
+                className="inline-block px-2 bg-indigo-700 mr-2 text-white text-sm rounded-full"
+              >
+                {language}
+              </span>
+            ))}
+          </h2>
+        </section>
+
+        <div className="relative h-auto w-96 shadow-md">
+          <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
+        </div>
+      </article>
+    </section>
+  );
+}
