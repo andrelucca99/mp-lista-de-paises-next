5c3c201beb32278196b8b08f0e160823aa6e9192
Andre Lucas <andrelucca99@gmail.com>
2023-10-30 14:42:14 -0300

    refactor: passando dados para CountryCard

--- app/pais/[name]/page.tsx
+++ app/pais/[name]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Country } from "@/app/page";
 import Link from "next/link";
 import Image from "next/image";
+import CountryCard from "@/components/country-card";
 
 async function getCountryByName(name: string): Promise<Country> {
   const response = await fetch("https://restcountries.com/v3.1/all");
@@ -10,12 +11,32 @@ async function getCountryByName(name: string): Promise<Country> {
   return countries.find((country: Country) => country.name.common == name)!;
 }
 
+async function getCountryBordersByName(name: string) {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const countries: Country[] = await response.json();
+
+  const country = countries.find((country: Country) => country.name.common == name)!;
+
+  return country.borders?.map(border => {
+    const borderCountry = countries.find(country => country.cca3 == border)!;
+    return {
+      name: borderCountry.name.common,
+      ptName: borderCountry.translations.por.common,
+      flag: borderCountry.flags.svg,
+      flagAlt: borderCountry.flags.alt
+    }
+  })
+}
+
 export default async function CountryPage({
   params: { name },
 }: {
   params: { name: string };
 }) {
   const country = await getCountryByName(decodeURI(name));
+  const borderCountries = await getCountryBordersByName(decodeURI(name));
+
+  console.log(borderCountries)
 
   const formatter = Intl.NumberFormat("en", { notation: "compact" });
 
@@ -59,6 +80,16 @@ export default async function CountryPage({
           <Image src={country.flags.svg} alt={country.flags.alt} fill className="object-cover" />
         </div>
       </article>
+      <section>
+        <h3 className="mt-12 text-2xl font-semibold text-gray-800">
+          Países que fazem fronteira
+        </h3>
+        <div className="grid grid-cols-5 gap-3 my-3 w-full">
+          {borderCountries?.map((border) => (
+              <CountryCard key={border.name} {...border}/>
+          ))}
+        </div>
+      </section>
     </section>
   );
 }
